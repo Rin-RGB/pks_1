a8cd2455251ca511f45b546aea7b8dbd92ed8484
Zhuleva Daria <zhuleva.d.a@edu.mirea.ru>
2026-03-03 18:02:19 +0300

    KR1 added

--- hello.h
+++ hello.h
@@ -1,7 +1,7 @@
 #ifndef HELLO_H
 #define HELLO_H
 
-#include <ostream>
+#include <ostream>  
 
 void printHelloWorld(std::ostream&);
 
--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 #include <iostream>
 
 int main() {
-    printHelloWorld(std::cout); 
+    printHelloWorld(std::cout);
     return 0;
 }
